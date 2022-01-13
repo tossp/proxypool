1e52ee187af93796063b0a7f068201cfc46a519b
bineyond <bineyond@gmail.com>
2022-01-14 00:08:56 +0800

    add assets.go

---
+++ config/assets.go
@@ -0,0 +1,13 @@
+package config
+
+import "embed"
+
+//go:embed assets/html/*
+var HtmlFs embed.FS
+
+//go:embed assets/static/*
+var StaticFS embed.FS
+
+//go:embed assets/Country.mmdb
+//go:embed assets/flags.json
+var GeoIpFS embed.FS
