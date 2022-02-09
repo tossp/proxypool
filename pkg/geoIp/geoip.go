36500e0bfe187b0e9c593c57e28b88cf48a7b3d5
bineyond <bineyond@gmail.com>
2022-02-09 14:47:05 +0800

    geoip 使用 lite 版本

--- pkg/geoIp/geoip.go
+++ pkg/geoIp/geoip.go
@@ -70,7 +70,7 @@ func NewGeoIP(geodb, flags string) (geoip GeoIP) {
 	db, err := geoip2.Open(geodb)
 	if err != nil {
 		// log.Println(err)
-		buf, err := GeoIpBinary(config.Config.GeoipDbUrl + "Country.mmdb")
+		buf, err := GeoIpBinary(config.Config.GeoipDbUrl + "lite/Country.mmdb")
 		if err != nil {
 			panic(err)
 		}
@@ -194,7 +194,7 @@ func UpdateGeoIP() {
 	}
 	if GeoIpDBCurVersion != ver {
 		// log.Println(err)
-		buf, err := GeoIpBinary(config.Config.GeoipDbUrl + "Country.mmdb")
+		buf, err := GeoIpBinary(config.Config.GeoipDbUrl + "lite/Country.mmdb")
 		if err != nil {
 			log.Errorln("GeoIpBinary: %v", err)
 			return
